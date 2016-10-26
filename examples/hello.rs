e07b85acc720e15b2892d0d1337c7763f0b20288
nokaa <nokaa@cock.li>
2016-10-26 17:19:28 -0500

    Rename to hayakuuuu

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,7 +1,7 @@
-extern crate http;
+extern crate hayaku;
 extern crate regex;
 
-use http::{Http, Request, ResponseWriter};
+use hayaku::{util, Http, Request, ResponseWriter};
 use regex::Regex;
 
 use std::rc::Rc;
@@ -14,9 +14,5 @@ fn main() {
 }
 
 fn hello_handler(_req: &Request, res: &mut ResponseWriter, _ctx: &()) {
-    res.status(200, "OK");
-    res.add_chunked().unwrap();
-    if res.done_headers().unwrap() {
-        res.write_body(b"Hello, world!");
-    }
+    util::send_string(res, b"Hello, world!");
 }
