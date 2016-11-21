e66fcee523b62814fbdb04d61937b2191287c145
nokaa <nokaa@cock.li>
2016-11-20 18:19:46 -0600

    Add comments, split off repeated code.

--- hayaku-path/src/trie/mod.rs
+++ hayaku-path/src/trie/mod.rs
@@ -18,50 +18,53 @@ impl<T: Clone> TrieNode<T> {
         }
     }
 
-    fn new_child(key: String, value: Option<T>) -> Self {
-        TrieNode {
+    /// Create a new child node with the given key-value pair and add it
+    /// as a child to node `self`.
+    fn add_new_child(&mut self, key: String, value: Option<T>) {
+        let child = TrieNode {
             key: key,
             value: value,
             children: Vec::new(),
-        }
+        };
+        self.children.push(Box::new(child));
     }
 
     /// Inserts a key-value pair into the trie.
     pub fn insert<S: Into<String>>(&mut self, key: S, value: T) {
         let key = key.into();
+        // Empty tree, simply set key/value for this node to given key/value.
         if self.key.is_empty() {
             self.key = key;
             self.value = Some(value);
         } else {
+            // Non-empty tree
+            // Get the length of the match for our nodes
+            // NOTE: The length of the match should always be
+            // at least 1. We disallow routes that do not start
+            // with '/'.
             let match_len = get_match_len(&self.key, &key);
+            // If the length of the match is the length of this node's key,
+            // we do not need to split the node.
             if match_len == self.key.len() {
                 let key = key[match_len..].to_string();
                 // This failing implies that we were given two of the same key
                 assert!(!key.is_empty());
+                // If there are no children, we just add a new node. No need to
+                // worry about another node with a matching prefix.
                 if self.children.is_empty() {
-                    let child = Self::new_child(key, Some(value));
-                    self.children.push(Box::new(child));
+                    self.add_new_child(key, Some(value));
                 } else {
-                    for mut child in &mut self.children {
-                        if child.is_match(&key) {
-                            child.insert(key, value);
-                            return;
-                        }
-
-                    }
-                    // No matching node found, add new child
-                    let child = Self::new_child(key, Some(value));
-                    self.children.push(Box::new(child));
+                    self.insert_children(key, value);
                 }
             } else {
+                // Match length was less than the length of this node's key.
                 // Split node into two seperate nodes
                 let child_key = self.key[match_len..].to_string();
                 self.key = self.key[0..match_len].to_string();
                 // TODO(nokaa): Cloning should be fine since this is an Rc
                 let child_value = self.value.clone();
-                let child = Self::new_child(child_key, child_value);
+                self.add_new_child(child_key, child_value);
                 self.value = None;
-                self.children.push(Box::new(child));
 
                 // Insert new node
                 let key = key[match_len..].to_string();
@@ -69,21 +72,26 @@ impl<T: Clone> TrieNode<T> {
                 assert!(!key.is_empty());
 
                 if self.children.is_empty() {
-                    let child = Self::new_child(key, Some(value));
-                    self.children.push(Box::new(child));
+                    self.add_new_child(key, Some(value));
                 } else {
-                    for mut child in &mut self.children {
-                        if child.is_match(&key) {
-                            child.insert(key, value);
-                            return;
-                        }
-                    }
-                    // No matching node found, add new child
-                    let child = Self::new_child(key, Some(value));
-                    self.children.push(Box::new(child));
+                    self.insert_children(key, value);
                 }
             }
         }
+    }
+
+    fn insert_children(&mut self, key: String, value: T) {
+        // Check all children of this node for one that has a
+        // common prefix of any length. If a common prefix is
+        // found, we  insert at that node.
+        for mut child in &mut self.children {
+            if child.is_match(&key) {
+                child.insert(key, value);
+                return;
+            }
+        }
+        // No matching node found, add new child
+        self.add_new_child(key, Some(value));
     }
 
     /// Determines if key matches this node.
@@ -142,7 +150,11 @@ mod test {
         let trie2 = TrieNode {
             key: "/".to_string(),
             value: Some("Data"),
-            children: vec![Box::new(TrieNode::new_child("2".to_string(), Some("Data2")))],
+            children: vec![Box::new(TrieNode {
+                               key: "2".to_string(),
+                               value: Some("Data2"),
+                               children: Vec::new(),
+                           })],
         };
 
         assert_eq!(trie, trie2);
@@ -157,8 +169,16 @@ mod test {
         let trie2 = TrieNode {
             key: "/".to_string(),
             value: None,
-            children: vec![Box::new(TrieNode::new_child("1".to_string(), Some("Data"))),
-                           Box::new(TrieNode::new_child("2".to_string(), Some("Data2")))],
+            children: vec![Box::new(TrieNode {
+                               key: "1".to_string(),
+                               value: Some("Data"),
+                               children: Vec::new(),
+                           }),
+                           Box::new(TrieNode {
+                               key: "2".to_string(),
+                               value: Some("Data2"),
+                               children: Vec::new(),
+                           })],
         };
 
         assert_eq!(trie, trie2);
