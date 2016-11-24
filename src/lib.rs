27bec21c3fdf9165513c84b438df39e8791c66eb
nokaa <nokaa@cock.li>
2016-11-24 01:51:24 -0600

    Add function to HTML-escape input string

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 extern crate hayaku_http;
 extern crate hayaku_path;
+extern crate marksman_escape;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
@@ -10,7 +11,13 @@ use std::collections::HashMap;
 
 pub use hayaku_http::{Http, Request, ResponseWriter, Method, Status};
 pub use hayaku_path::Router;
+use marksman_escape::Escape;
 
 pub fn get_path_params(req: &Request) -> HashMap<String, String> {
     serde_json::from_slice(&*req.user_data.borrow()).unwrap()
+}
+
+pub fn escape_html(input: &str) -> String {
+    let escaped = Escape::new(input.bytes()).collect();
+    String::from_utf8(escaped).unwrap()
 }
