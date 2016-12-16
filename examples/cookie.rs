0525b04ecdae18f86379af59c0be2346ff6c7179
nokaa <nokaa@cock.li>
2016-12-16 08:51:26 -0600

    Update cookie example

--- examples/cookie.rs
+++ examples/cookie.rs
@@ -1,7 +1,10 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate hayaku;
 
-use hayaku::{Http, Router, Request, ResponseWriter, Status, Cookie};
-use std::rc::Rc;
+use hayaku::{Http, Router, Request, Response, Status, Cookie};
+use std::sync::Arc;
 
 #[derive(Clone)]
 struct Context {
@@ -10,26 +13,27 @@ struct Context {
 }
 
 fn main() {
+    env_logger::init();
     let ctx = Context {
         username: "username".to_string(),
         password: "password".to_string(),
     };
 
     let mut router = Router::new();
-    router.get("/", Rc::new(home_handler)).unwrap();
-    router.post("/login", Rc::new(login_handler)).unwrap();
-    router.get("/secret", Rc::new(secret_handler)).unwrap();
+    router.get("/", Arc::new(home_handler)).unwrap();
+    router.post("/login", Arc::new(login_handler)).unwrap();
+    router.get("/secret", Arc::new(secret_handler)).unwrap();
 
     let addr = "127.0.0.1:3000".parse().unwrap();
     let http = Http::new(router, ctx);
     http.listen_and_serve(addr);
 }
 
-fn home_handler(_req: &Request, res: &mut ResponseWriter, _ctx: &Context) {
-    res.send_file("examples/login.html");
+fn home_handler(_req: &Request, res: &mut Response, _ctx: &Context) {
+    res.send_file("examples/assets/login.html");
 }
 
-fn login_handler(req: &Request, res: &mut ResponseWriter, ctx: &Context) {
+fn login_handler(req: &Request, res: &mut Response, ctx: &Context) {
     let username = req.form_value("username").unwrap();
     let password = req.form_value("password").unwrap();
 
@@ -37,14 +41,21 @@ fn login_handler(req: &Request, res: &mut ResponseWriter, ctx: &Context) {
         let p = "/".to_string();
         let cookie = Cookie::new("loggedin", "true").path(p);
         res.set_cookie(&cookie);
-        res.redirect(Status::Found, b"/secret", b"");
+        res.redirect(Status::Found, "/secret", b"");
     } else {
-        res.redirect(Status::Found, b"/", b"Incorrect login");
+        res.redirect(Status::Found, "/", b"Incorrect login");
     }
 }
 
-fn secret_handler(req: &Request, res: &mut ResponseWriter, _ctx: &Context) {
+fn secret_handler(req: &Request, res: &mut Response, _ctx: &Context) {
     let cookies = req.get_cookies();
-    println!("cookies: {:?}", cookies);
-    res.send_file("examples/secret.html");
+    info!("cookies: {:?}", cookies);
+    for cookie in cookies {
+        if cookie.name == "loggedin" && cookie.value == "true" {
+            res.send_file("examples/assets/secret.html");
+            return;
+        }
+    }
+
+    res.redirect(Status::Found, "/", b"Incorrect login");
 }
