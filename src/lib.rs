7f5d33eab5a0129f4631c0372ee5a1cad6787194
nokaa <nokaa@cock.li>
2017-01-09 15:27:45 -0600

    Mv get_path_params to hayaku-path, rm unused deps

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,11 @@ extern crate cookie;
 extern crate hayaku_http;
 extern crate hayaku_path;
 extern crate marksman_escape;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-
-use std::collections::HashMap;
 
 pub use cookie::Cookie;
 pub use hayaku_http::{Header, Http, Request, Response, Method, Status};
-pub use hayaku_path::Router;
+pub use hayaku_path::{Router, get_path_params};
 use marksman_escape::Escape;
-
-pub fn get_path_params(req: &Request) -> HashMap<String, String> {
-    serde_json::from_slice(&*req.user_data.borrow()).unwrap()
-}
 
 pub fn escape_html(input: &str) -> String {
     let escaped = Escape::new(input.bytes()).collect();
